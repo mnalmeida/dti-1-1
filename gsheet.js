9bba125228a25674807688bc58eaea472300d125
Matheus Araujo <matheus.saraujo@gmail.com>
2019-01-19 09:35:18 -0200

    tribe filter

--- gsheet.js
+++ gsheet.js
@@ -1,5 +1,12 @@
 "use strict";
 
+const graphBegin = "digraph G { \n\tedge [dir = none]\n\tnode [shape = circle]\n";
+const graphEnd = "}"
+
+var graphStr = null;
+var nodes = [];
+var edges = [];
+
 if (!String.prototype.format) {
     String.prototype.format = function() {
     var args = arguments;
@@ -9,19 +16,97 @@ if (!String.prototype.format) {
     };
 }
 
-const graphBegin = "digraph G { \nedge [dir = none]\nnode [shape = circle]\n";
-const graphEnd = "}"
+function gSheetDoData(json) {
+    try {
+        var tribe = document.querySelector("#tribe select").value;                
+        gSheetCreateNodesAndEdges(json.feed.entry, tribe);
+        graphStr = "{0}{1}{2}{3}".format(graphBegin, gSheetCreateNodesStr(), gSheetCreateEdgesStr(), graphEnd);
+        console.log(graphStr);
+    }
+    catch(err) {
+        console.error(err);
+        return null;
+    }
+}
 
-var graphStr = null;
+function gSheetGetData() {    
+    var i = 0;
+    while(!graphStr && i < 100) 
+        i++;
+    return graphStr;
+}
 
-function getNodeLabel(name) {
+function gSheetCreateNodesStr() {
+    var graphNodes = "";
+    for(var i = 0; i < nodes.length; i++) {
+        graphNodes += gSheetCreateNodeStr(nodes[i]);
+    }
+    return graphNodes;
+}
+
+function gSheetCreateEdgesStr() {
+    var graphEdges = "";
+    for(var i = 0; i < edges.length; i++) {
+        graphEdges += "\t{0} -> {1}; \n".format(edges[i].source.replace('.', ''), edges[i].target.replace('.', ''));
+    }
+    return graphEdges;
+}
+
+function gSheetCreateNodesAndEdges(data, selectedTribe) {
+    
+    nodes = [];
+    edges = [];
+
+    for(var r=4; r<data.length; r+=4) {
+        var source = data[r]["gs$cell"]["$t"].toLowerCase();
+        var target = data[r+1]["gs$cell"]["$t"].toLowerCase();
+        var date = data[r+2]["gs$cell"]["$t"].toLowerCase();
+        var tribe = data[r+3]["gs$cell"]["$t"].toLowerCase();
+
+        if (selectedTribe === "dti" || selectedTribe === tribe) {
+            gSheetCreateNode(source, target, date);
+            gSheetCreateEdge(source, target);        
+        }
+    }
+
+}
+
+function gSheetCreateNode(source, target, date) {
+    if (!nodes.some(n => n.id === source)) {
+        nodes.push({ 
+            id: source, 
+            label: gSheetGetNodeLabel(source),
+            color: gSheetGetNodeColor(date)
+        });
+    }
+    else {
+        var i = nodes.findIndex(n => n.id === source);                
+        if (i) {
+            nodes[i].color = gSheetGetNodeColor(date);
+        }
+    }
+    
+    if (!nodes.some(n => n.id === target)) {
+        nodes.push({ 
+            id: target,
+            label: gSheetGetNodeLabel(target),
+            color: 'gray'
+        });
+    }
+}
+
+function gSheetCreateEdge(source, target) {
+    edges.push({source: source, target: target});
+}
+
+function gSheetGetNodeLabel(name) {
     var names = name.toUpperCase().split('.');
     if (names.length === 2)
         return names[0][0] + names[1][0];
     return name[0];
 }
 
-function getNodeColor(date) {
+function gSheetGetNodeColor(date) {
     var date1 = new Date(date);
     var date2 = new Date();
     var timeDiff = Math.abs(date2.getTime() - date1.getTime());
@@ -35,74 +120,8 @@ function getNodeColor(date) {
         return "red";
 }
 
-function createNode(node) {    
-    return "{0} [style=filled, label=\"{1}\", color={2}, fillcolor={2}]; \n".format(node.id.replace('.', ''), node.label, node.color);
-}
-
-function gSheetDoData(json) {
-
-    try {
-
-        var data = json.feed.entry;
-        var graphNodes = "";
-        var graphEdges = "";
-
-        var nodes = [];
-        var edges = [];
-        
-        for(var r=4; r<data.length; r+=4) {
-            var source = data[r]["gs$cell"]["$t"].toLowerCase();
-            var target = data[r+1]["gs$cell"]["$t"].toLowerCase();
-            var date = data[r+2]["gs$cell"]["$t"].toLowerCase();
-
-            if (!nodes.some(n => n.id === source)) {
-                nodes.push({ 
-                    id: source, 
-                    label: getNodeLabel(source),
-                    color: getNodeColor(date)
-                });
-            }
-            else {
-                var i = nodes.findIndex(n => n.id === source);                
-                if (i) {
-                    nodes[i].color = getNodeColor(date);
-                }
-            }
-            
-            if (!nodes.some(n => n.id === target)) {
-                nodes.push({ 
-                    id: target,
-                    label: getNodeLabel(target),
-                    color: 'gray'
-                });
-            }
-
-            edges.push({source: source, target: target});
-        }
-
-        for(var i = 0; i < nodes.length; i++) {
-            graphNodes += createNode(nodes[i]);
-        }
-
-        for(var i = 0; i < edges.length; i++) {
-            graphEdges += "{0} -> {1}; \n".format(edges[i].source.replace('.', ''), edges[i].target.replace('.', ''));
-        }
-
-        graphStr = "{0}{1}{2}{3}".format(graphBegin, graphNodes, graphEdges, graphEnd);
-        console.log(graphStr);
-
-    }
-    catch(err) {
-        console.log(err);
-        return null;
-    }
-}
-
-function gSheetGetData() {    
-    var i = 0;
-    while(!graphStr && i < 100) 
-        i++;
-    return graphStr;
+function gSheetCreateNodeStr(node) {    
+    return "\t{0} [style=filled, label=\"{1}\", color={2}, fillcolor={2}]; \n".format(node.id.replace('.', ''), node.label, node.color);
 }
 
 function gSheetClear() {
