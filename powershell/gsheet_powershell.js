759c712b3c9841ef54e0523cfd43a5e7335adb7d
Matheus Araujo <matheus.saraujo@gmail.com>
2019-02-04 23:36:39 -0200

    powershell criado

--- powershell/gsheet_powershell.js
+++ powershell/gsheet_powershell.js
@@ -9,7 +9,7 @@ graphBegin += "\t\tleng2 [label=\"<=21d\", style=filled, color=\"#93E247\", fill
 graphBegin += "\t\tleng3 [label=\"<=28d\", style=filled, color=\"#FFC239\", fillcolor=\"#FFC239\"];\n";
 graphBegin += "\t\tleng4 [label=\"<=35d\", style=filled, color=\"#EA7439\", fillcolor=\"#EA7439\"];\n";
 graphBegin += "\t\tleng5 [label=\">35d\", style=filled, color=\"#ED1E2E\", fillcolor=\"#ED1E2E\"];\n";
-graphBegin += "\t\tleng6 [label=\"ñ def\", style=filled, color=\"gray\", fillcolor=\"gray\"];\n";
+graphBegin += "\t\tleng6 [label=\"n def\", style=filled, color=\"gray\", fillcolor=\"gray\"];\n";
 graphBegin += "\t\tlabel = \"legenda\";\n";
 graphBegin += "\t}\n";
 var graphEnd = "}";
@@ -19,35 +19,35 @@ var nodes = [];
 var edges = [];
 
 if (!String.prototype.format) {
-    String.prototype.format = function() {
-    var args = arguments;
-    return this.replace(/{(\d+)}/g, function(match, number) { 
-        return typeof args[number] != 'undefined' ? args[number] : match;
+    String.prototype.format = function () {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function (match, number) {
+            return typeof args[number] != 'undefined' ? args[number] : match;
         });
     };
 }
 
 function gSheetDoData(json) {
     try {
-        var selectedTribe = document.querySelector("#tribe select").value;                
+        var selectedTribe = "dti";        
         gSheetCreateNodesAndEdges(json.feed.entry, selectedTribe);
         graphStr = "{0}{1}{2}{3}{4}".format(
             graphBegin,
             gSheetCreateTribeNameStr(selectedTribe),
-            gSheetCreateNodesStr(), 
-            gSheetCreateEdgesStr(selectedTribe), 
+            gSheetCreateNodesStr(),
+            gSheetCreateEdgesStr(selectedTribe),
             graphEnd);
         console.log(graphStr);
     }
-    catch(err) {
+    catch (err) {
         console.error(err);
         return null;
     }
 }
 
-function gSheetGetData() {    
+function gSheetGetData() {
     var i = 0;
-    while(!graphStr && i < 100) 
+    while (!graphStr && i < 100)
         i++;
     return graphStr;
 }
@@ -58,7 +58,7 @@ function gSheetCreateTribeNameStr(tribe) {
 
 function gSheetCreateNodesStr() {
     var graphNodes = "";
-    for(var i = 0; i < nodes.length; i++) {
+    for (var i = 0; i < nodes.length; i++) {
         graphNodes += gSheetCreateNodeStr(nodes[i]);
     }
     return graphNodes;
@@ -66,9 +66,9 @@ function gSheetCreateNodesStr() {
 
 function gSheetCreateEdgesStr(selectedTribe) {
     var graphEdges = "";
-    for(var i = 0; i < edges.length; i++) {
+    for (var i = 0; i < edges.length; i++) {
         graphEdges += "\t{0} -> {1} [color=\"{2}\", penwidth={3}]; \n".format(
-            edges[i].source.replace('.', ''), 
+            edges[i].source.replace('.', ''),
             edges[i].target.replace('.', ''),
             gSheetGetEdgeColor(edges[i].tribe),
             selectedTribe === 'dti' ? '3' : '1'
@@ -78,19 +78,19 @@ function gSheetCreateEdgesStr(selectedTribe) {
 }
 
 function gSheetCreateNodesAndEdges(data, selectedTribe) {
-    
+
     nodes = [];
     edges = [];
 
-    for(var r=4; r<data.length; r+=4) {
+    for (var r = 4; r < data.length; r += 4) {
         var source = data[r]["gs$cell"]["$t"].toLowerCase();
-        var target = data[r+1]["gs$cell"]["$t"].toLowerCase();
-        var date = data[r+2]["gs$cell"]["$t"].toLowerCase();
-        var tribe = data[r+3]["gs$cell"]["$t"].toLowerCase();
+        var target = data[r + 1]["gs$cell"]["$t"].toLowerCase();
+        var date = data[r + 2]["gs$cell"]["$t"].toLowerCase();
+        var tribe = data[r + 3]["gs$cell"]["$t"].toLowerCase();
 
         if (selectedTribe === "dti" || selectedTribe === tribe) {
             gSheetCreateNode(source, target, date);
-            gSheetCreateEdge(source, target, tribe);        
+            gSheetCreateEdge(source, target, tribe);
         }
     }
 
@@ -98,21 +98,21 @@ function gSheetCreateNodesAndEdges(data, selectedTribe) {
 
 function gSheetCreateNode(source, target, date) {
     if (!nodes.some(n => n.id === source)) {
-        nodes.push({ 
-            id: source, 
+        nodes.push({
+            id: source,
             label: gSheetGetNodeLabel(source),
             color: gSheetGetNodeColor(date)
         });
     }
     else {
-        var i = nodes.findIndex(n => n.id === source);                
+        var i = nodes.findIndex(n => n.id === source);
         if (i) {
             nodes[i].color = gSheetGetNodeColor(date);
         }
     }
-    
+
     if (!nodes.some(n => n.id === target)) {
-        nodes.push({ 
+        nodes.push({
             id: target,
             label: gSheetGetNodeLabel(target),
             color: 'gray'
@@ -121,7 +121,7 @@ function gSheetCreateNode(source, target, date) {
 }
 
 function gSheetCreateEdge(source, target, tribe) {
-    edges.push({source: source, target: target, tribe: tribe});
+    edges.push({ source: source, target: target, tribe: tribe });
 }
 
 function gSheetGetNodeLabel(name) {
@@ -135,9 +135,9 @@ function gSheetGetNodeColor(date) {
     var date1 = new Date(date);
     var date2 = new Date();
     var timeDiff = Math.abs(date2.getTime() - date1.getTime());
-    var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-    
-    if (diffDays <= 14) 
+    var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+    if (diffDays <= 14)
         return '#09AA51';
     if (diffDays <= 21)
         return '#93E247';
@@ -173,7 +173,7 @@ function gSheetGetEdgeColor(tribe) {
     return '#000000';
 }
 
-function gSheetCreateNodeStr(node) {    
+function gSheetCreateNodeStr(node) {
     return "\t{0} [style=filled, label=\"{1}\", color=\"{2}\", fillcolor=\"{2}\"]; \n".format(node.id.replace('.', ''), node.label, node.color);
 }
 
@@ -181,4 +181,16 @@ function gSheetClear() {
     graphStr = null;
 }
 
-// reference: https://gist.github.com/terrywbrady/a03b25fe42959b304b1e
+const https = require('https');
+
+https.get('https://spreadsheets.google.com/feeds/cells/18VQygZXodUcmkli9RQjccoUY8qUfw6Vf5B9PChnmZKI/1/public/values?alt=json', (resp) => {
+  let data = '';
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+  resp.on('end', () => {
+    gSheetDoData(JSON.parse(data));
+  });
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
--- powershell/update.ps1
+++ powershell/update.ps1
@@ -6,7 +6,8 @@ function Copy-Local2DropBox {
         [string]$TargetFilePath
     )
 
-    $arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
+    #$arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
+    $arg = '{ "path": "' + $TargetFilePath + '", "mode": "overwrite", "autorename": true, "mute": false }'
     $authorization = "Bearer " + (get-item env:DropBoxAccessToken).Value
     
     $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
@@ -17,14 +18,48 @@ function Copy-Local2DropBox {
     Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
 }
 
-function Get-DotCode {
-    param($query)
-
-    $url = "http://bing.com?q=flight status for $query"
-
-    $result = Invoke-WebRequest $url
-
-    $result.AllElements | 
-        Where Class -eq "ans" |
-        Select -First 1 -ExpandProperty innerText    
+function Run-JavaScript {
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string]$SourceFilePath,
+        [Parameter(Mandatory=$true)]
+        [string]$OutputFile
+    )
+    node $SourceFilePath > $OutputFile
 }
+
+function Set-Encoding {
+    Param (
+        [Parameter(Mandatory=$true)]
+        [string]$SourceFilePath,
+        [Parameter(Mandatory=$true)]
+        [string]$OutputFile
+    )
+    Get-Content $SourceFilePath | Out-File -Force -Encoding ASCII $OutputFile    
+}
+
+function Run-Graphviz {
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string]$SourceFilePath,
+        [Parameter(Mandatory=$true)]
+        [string]$OutputFile
+    )
+    fdp -Tpng $SourceFilePath -o $OutputFile -Lg
+}
+
+function Set-ImageSize {
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string]$SourceFilePath,
+        [Parameter(Mandatory=$true)]
+        [string]$OutputFile
+    )    
+    Resize-Image -InputFile $SourceFilePath -Scale 30 -OutputFile $OutputFile
+}
+
+Run-JavaScript '.\gsheet_powershell' 'dti-1-1-tmp.dot'
+Set-Encoding 'dti-1-1-tmp.dot' 'dti-1-1.dot'
+Run-Graphviz 'dti-1-1.dot' 'dti-1-1.png'
+# Resize image
+Copy-Local2DropBox 'dti-1-1.png' '/public/download.png'
